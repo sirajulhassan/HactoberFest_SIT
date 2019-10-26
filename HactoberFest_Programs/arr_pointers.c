50273d2e698395158cca176ce191cb8e2e5be180
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2019-10-26 11:11:18 +0530

    Updated by adding calloc()
    Displayed the address of all the elements using pointer

--- HactoberFest_Programs/arr_pointers.c
+++ HactoberFest_Programs/arr_pointers.c
@@ -1,7 +1,8 @@
+#include <stdlib.h>
 #include<stdio.h>
 int main() {
-	int arr[]={1,2,3,4,5}; 
-	//Declare a pointer ptr of type int
-	printf("\n Address of array = %p %p %p %p", arr, &arr[0], &arr,* p+2);
-	return 0;
+        int arr[]={1,2,3,4,5}; 
+        int *p=(int*)calloc(sizeof(arr)/sizeof(arr[0]),sizeof(int));
+        printf("\n Address of array = %p %p %p %p %p",(p+0),(p+1),(p+2),(p+3),(p+4));
+        return 0;
 } 

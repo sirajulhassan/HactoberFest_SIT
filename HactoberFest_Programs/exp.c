6ff303542dcb993ee1769f15fd9dfb2547a94bd1
sirajulhassan <57223268+sirajulhassan@users.noreply.github.com>
2019-10-31 06:37:52 -0700

    update exp.c
    we can't use system defined keywords name as a function name

--- HactoberFest_Programs/exp.c
+++ HactoberFest_Programs/exp.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int exp(int a,int b){
-	if(b==0)
+int expo(int a,int b){
+	if(b<=0)
 		return 1;
 	else
-		return a*exp(a,b-1);
+		return a*expo(a,b-1);
 }
 int main(){
 	int num1,num2,res=0;
 	printf("\nEnter 2 numbers : ");
 	scanf("%d%d",&num1,&num2);
-	res=exp(num1,num2);
+	res=expo(num1,num2);
 	printf("\nExponent of %d^%d is : %d\n",num1,num2,res);
 	return 0;
 }
